cfbd045b5e5e2e1198c1545153bda88bd675ede3
ahmedshahat921-cpu <ahmedshahat921@gmail.com>
2026-02-20 07:14:38 +0200

    fix: Return JSON error messages from server

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/register', async (req, res) => {
         res.status(201).json({ msg: 'User registered successfully' });
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Server Error');
+        res.status(500).json({ msg: 'Server Error' });
     }
 });
 
@@ -94,7 +94,7 @@ app.post('/login', async (req, res) => {
         res.json({ msg: 'Login successful', username: user.username });
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Server Error');
+        res.status(500).json({ msg: 'Server Error' });
     }
 });
 
